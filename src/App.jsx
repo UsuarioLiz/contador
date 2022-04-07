2ac25ab8b8817f3d09821cfd9cf27cce8e6b29c5
Lizbeth Garcia Gil <lizbethgg07@gmail.com>
2022-04-07 12:38:43 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function App() {
+  const [cuenta, setCuenta] = useState(0);
+
+  const handleClick = () => {
+    console.log("hola")
+    setCuenta(cuenta + 1);
+  };
+
+  return (
+    <div className="App">
+      <h3>Contador</h3>
+      <hr />
+      <h2 className="text-center">{cuenta}</h2>
+      <hr />
+      <div
+        style={{
+          display:"flex",
+          justifyContent: "flex-end",
+          marginRight:"5px"
+        }}
+      >
+        <button type="button" className="btn btn-primary" onClick={handleClick}>
+          +1
+        </button>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
